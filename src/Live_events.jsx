8a93b2fdbc644b04a36ec70c71552037bc133304
Ankur0310 <ankurknp0310@gmail.com>
2022-10-28 12:29:04 +0530

    Home part1 commit

---
+++ src/Live_events.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Live_events() {
+    return (
+      <h1>hello from live events</h1>
+      
+    );
+  }
+  
+  export default Live_events;
---
+++ src/Notice.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Notice() {
+    return (
+      <h1>hello from notice</h1>
+      
+    );
+  }
+  
+  export default Notice;
