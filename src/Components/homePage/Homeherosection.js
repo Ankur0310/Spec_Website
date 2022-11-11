00938eeca93a39a001d7827cd8ef0dcfa361150e
anmolchhabra21 <anmolchhabra71500@gmail.com>
2022-11-12 00:56:54 +0530

    commits

--- src/Components/homePage/Homeherosection.js
+++ src/Components/homePage/Homeherosection.js
@@ -1,31 +1,41 @@
 import React from "react";
+import { useRef } from "react";
 import "../homePage/Homeherosection.css";
-var current_page = 1;
-var last_page = 4;
-var step = 0, x = 0;
-var play = false;
-const wraper = document.getElementsByClassName('superScroll');
-
-var SupScroll =(e)=>{
-  
-  if(e.deltaY < 0 && !play){
-    play = true;
-    current_page--;
-    step-=100;
-    console.log(e.deltaY,step);
-  }
-  else if(e.deltaY>0 && !play){
-    play = true;
-    current_page++;
-    step+=100;
-    wraper.style.transform = `translateY(${step}vh)`;
-    console.log(e.deltaY,step);
-  }
-}
 
 function Homeherosection() {
+  const piyush = useRef();
+  var current_page = 1;
+  var last_page = 4;
+  var step = 0, x = 0;
+  var play = false;
+  // const wraper = document.getElementsByClassName("superScroll")[0];
+  // console.log(piyush);
+  // console.log(piyush);
+  
+  // wraper.addEventListener("wheel",SupScroll,false);
+  
+  var SupScroll =(e)=>{
+    
+    if(e.deltaY < 0 && !play){
+      // play = true;
+      current_page--;
+      step-=100;
+      console.log(e.deltaY,step);
+      e.preventDefault();
+    }
+    else if(e.deltaY > 0 && !play){
+      // play = true;
+      current_page++;
+      step+=100;
+      // wraper.style.transform = `translateY(${step}vh)`;
+      console.log(e.deltaY,step);
+      e.preventDefault()
+    }
+  }
+
   return (
-    <div onWheel={(e)=>{SupScroll(e)}} className="superScroll">
+    <div className="superScroll" ref={piyush}>
+      {console.log(piyush)} 
       <div className="parallax-1">
         <div className="bosm-heading"></div>
           <div className="ph1"></div>
--- src/Components/sportsPage/Gallery.js
+++ src/Components/sportsPage/Gallery.js
@@ -1,59 +1,134 @@
+// import React from "react";
+// import "./Gallery.css";
+// import img3 from "../../img/s3.jpg";
+
+// const Gallery = () => {
+//   //     // Instantiate the Bootstrap carousel
+//   // $('.multi-item-carousel').carousel({
+//   //     interval: false
+//   //   });
+
+//   //   // for every slide in carousel, copy the next slide's item in the slide.
+//   //   // Do the same for the next, next item.
+//   //   $('.multi-item-carousel .item').each(function(){
+//   //     var next = $(this).next();
+//   //     if (!next.length) {
+//   //       next = $(this).siblings(':first');
+//   //     }
+//   //     next.children(':first-child').clone().appendTo($(this));
+
+//   //     if (next.next().length>0) {
+//   //       next.next().children(':first-child').clone().appendTo($(this));
+//   //     } else {
+//   //         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+//   //     }
+//   //   });
+//   return (
+//     <div className="gallery_main">
+//       <h1 className="Gallery_heading text-center">Gallery</h1>
+
+//       <div className="container">
+//         <div className="row">
+//           <div className="col-md-12 s_flex_cont">
+
+//             <div className="flex-item">
+//             <img src={img3} alt="" />
+//             </div>
+
+//             <div className="flex-item">
+//             <img src={img3} alt="" />
+//             </div>
+
+//             <div className="flex-item">
+//             <img src={img3} alt="" />
+//             </div>
+
+//             <div className="flex-item">
+//             <img src={img3} alt="" />
+//             </div>
+
+
+//           </div>
+//         </div>
+//       </div>
+//       <hr />
+//     </div>
+//   );
+// };
+
+// export default Gallery;
+
 import React from "react";
 import "./Gallery.css";
+import img1 from "../../img/s1.jpg";
+import img2 from "../../img/s2.jpg";
 import img3 from "../../img/s3.jpg";
+import img4 from "../../img/s4.jpg";
+import img5 from "../../img/s5.jpg";
+import img6 from "../../img/s6.jpg";
 
-const Gallery = () => {
-  //     // Instantiate the Bootstrap carousel
-  // $('.multi-item-carousel').carousel({
-  //     interval: false
-  //   });
-
-  //   // for every slide in carousel, copy the next slide's item in the slide.
-  //   // Do the same for the next, next item.
-  //   $('.multi-item-carousel .item').each(function(){
-  //     var next = $(this).next();
-  //     if (!next.length) {
-  //       next = $(this).siblings(':first');
-  //     }
-  //     next.children(':first-child').clone().appendTo($(this));
-
-  //     if (next.next().length>0) {
-  //       next.next().children(':first-child').clone().appendTo($(this));
-  //     } else {
-  //         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  //     }
-  //   });
+function Gallery() {
   return (
-    <div className="gallery_main">
-      <h1 className="Gallery_heading text-center">Gallery</h1>
+    
+    <div className="main_div_sports">
+      <h1 className="text-center">GALLERY</h1>
+      <div class="cards-list">
+  
+  <div class="card 1">
+    <div class="card_image">  <img src={img1} alt="" /> </div>
+    <div class="card_title title-white">
+      <p>Basketball</p>
+    </div>
+  </div>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 s_flex_cont">
-
-            <div className="flex-item">
-            <img src={img3} alt="" />
-            </div>
-
-            <div className="flex-item">
-            <img src={img3} alt="" />
-            </div>
-
-            <div className="flex-item">
-            <img src={img3} alt="" />
-            </div>
-
-            <div className="flex-item">
-            <img src={img3} alt="" />
-            </div>
-
-
-          </div>
-        </div>
+    <div class="card 2">
+    <div class="card_image">
+    <img src={img2} alt="" />
       </div>
-      <hr />
+    <div class="card_title title-white">
+      <p>Volley Ball</p>
+    </div>
+  </div>
+{/* 
+  <div class="card 3">
+    <div class="card_image">
+    <img src={img3} alt="" />
+    </div>
+    <div class="card_title title-white">
+      <p>Cricket</p>
+    </div>
+  </div> */}
+
+  <div class="card 4">
+  <div class="card_image">
+  <img src={img4} alt="" />
+    </div>
+  <div class="card_title title-white">
+    <p>Hockey</p>
+  </div>
+  </div>
+
+  <div class="card 5">
+  <div class="card_image">
+  <img src={img5} alt="" />
+    </div>
+  <div class="card_title title-white">
+    <p>Badminton</p>
+  </div>
+  </div>
+
+  <div class="card 6">
+  <div class="card_image">
+  <img src={img6} alt="" />
+    </div>
+  <div class="card_title title-white">
+    <p>Tennis</p>
+  </div>
+  </div>
+
+</div>
     </div>
   );
-};
+}
 
 export default Gallery;
