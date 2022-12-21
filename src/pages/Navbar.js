9efa644afc369d218c66c7ca490a0a3e6eaa45f6
ABHAY MITTAL <abhayhmittal@gmail.com>
2022-12-21 14:31:48 +0530

    x

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -11,16 +11,15 @@ const Navbar = () => {
       <nav className="main-nav">
         {/* 1st logo part  */}
         <div className="logo">
-          <h2>
-            SPEC
-          </h2>
+          <h2>SPEC</h2>
         </div>
 
         {/* 2nd menu part  */}
         <div
           className={
             showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
-          }>
+          }
+        >
           <ul>
             <li>
               {/* <NavLink to="/">Home</NavLink> */}
@@ -48,9 +47,9 @@ const Navbar = () => {
         <div className="social-media">
           {/* hamburget menu start  */}
           <div className="hamburger-menu">
-            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+            <button href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
               <GiHamburgerMenu />
-            </a>
+            </button>
           </div>
         </div>
       </nav>
